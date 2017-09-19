d162682f7aed00e02fb92f58a6a4fe8b9b80b736
Daniel Padfield <d.padfieldscfc@gmail.com>
2017-09-19 16:43:20 +0100

    Update form feedback graphs

--- R_form/form_feedback.R
+++ R_form/form_feedback.R
@@ -7,6 +7,7 @@ library(dplyr)
 library(ggplot2)
 library(ggimage)
 library(palettetown)
+library(stringr)
 
 # find file
 x <- drive_find(n_max = 10, type = c("spreadsheet", "jpg"))
@@ -74,9 +75,101 @@ ggsave(file.path('R_form', 'Experience_using_R.pdf'), last_plot(), width = 5.25,
 ggsave(file.path('R_form', 'Experience_using_R.png'), last_plot(), width = 5.25, height = 4.25)
 
 # look at what people want to learn ####
+learn <- select(d_use, R_learn, name) %>%
+  mutate(., R_learn = str_replace_all(R_learn, c('project,' = 'project', 'files,' = 'files'))) %>%
+  mutate(., hopes = strsplit(.$R_learn, ',')) %>%
+  unnest(., hopes) %>%
+  mutate(., hopes = trimws(hopes)) %>%
+  group_by(., hopes) %>%
+  summarise(n = n())
 
+ggplot(learn, aes(forcats::fct_reorder(hopes, n, .desc = TRUE), n, fill = hopes)) +
+  geom_bar(stat = 'identity') +
+  xlab('') +
+  ylab('Number of responses') +
+  theme_bw(base_size = 12, base_family = 'Helvetica') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 8),
+        legend.text = element_text(size = 8),
+        legend.title = element_blank()) +
+  labs(title = 'What do we want to learn in our R sessions!') +
+  scale_y_continuous(breaks = scales::pretty_breaks())
+
+ggsave(file.path('R_form', 'what_to_learn.pdf'), last_plot(), width = 5.5, height = 5)
+ggsave(file.path('R_form', 'what_to_learn.png'), last_plot(), width = 5.5, height = 5)
 
 # Used packages ####
+pkg_used <- select(d_use, pkg_used, name) %>%
+  mutate(., used = strsplit(.$pkg_used, ',')) %>%
+  unnest(., used) %>%
+  mutate(., used = trimws(used)) %>%
+  group_by(., used) %>%
+  summarise(n = n())
 
-# Packages used ####
+ggplot(pkg_used, aes(forcats::fct_reorder(used, n, .desc = TRUE), n, fill = used)) +
+  geom_bar(stat = 'identity') +
+  xlab('') +
+  ylab('Number of responses') +
+  theme_bw(base_size = 12, base_family = 'Helvetica') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 8),
+        legend.text = element_text(size = 8),
+        legend.title = element_blank()) +
+  labs(title = 'What packages have you used') +
+  scale_y_continuous(breaks = scales::pretty_breaks())
 
+ggsave(file.path('R_form', 'pkgs_used.pdf'), last_plot(), width = 5.5, height = 5)
+ggsave(file.path('R_form', 'pkgs_used.png'), last_plot(), width = 5.5, height = 5)
+
+# Favourite packages ####
+pkg_fave <- select(d_use, fave_pkgs, name) %>%
+  mutate(., fave_pkgs = str_replace_all(fixed(fave_pkgs), c('Ggplot2' = 'ggplot2', 'ggplot' = 'ggplot2', 'ddply' = 'plyr'))) %>%
+  mutate(., fave = strsplit(.$fave_pkgs, ',')) %>%
+  unnest(., fave) %>%
+  mutate(., fave = trimws(fave)) %>%
+  group_by(., fave) %>%
+  summarise(n = n())
+
+ggplot(pkg_fave, aes(forcats::fct_reorder(fave, n, .desc = TRUE), n, fill = fave)) +
+  geom_bar(stat = 'identity') +
+  xlab('') +
+  ylab('Number of responses') +
+  theme_bw(base_size = 12, base_family = 'Helvetica') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 8),
+        legend.text = element_text(size = 8),
+        legend.title = element_blank()) +
+  labs(title = 'What are your favourite packages?') +
+  scale_y_continuous(breaks = scales::pretty_breaks())
+
+ggsave(file.path('R_form', 'pkgs_fave.pdf'), last_plot(), width = 5.5, height = 5)
+ggsave(file.path('R_form', 'pkgs_fave.png'), last_plot(), width = 5.5, height = 5)
+
+# Current uses of RStudio ####
+curr_use <- select(d_use, R_use, other_tasks, name) %>%
+  gather(., 'id', 'use', c(R_use, other_tasks)) %>%
+  mutate(., use = strsplit(.$use, ',')) %>%
+  unnest(., use) %>%
+  mutate(., use = trimws(use)) %>%
+  group_by(., use) %>%
+  summarise(n = n()) %>%
+  filter(., !is.na(use))
+
+ggplot(curr_use, aes(forcats::fct_reorder(use, n, .desc = TRUE), n, fill = use)) +
+  geom_bar(stat = 'identity') +
+  xlab('') +
+  ylab('Number of responses') +
+  theme_bw(base_size = 12, base_family = 'Helvetica') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 8),
+        legend.text = element_text(size = 8),
+        legend.title = element_blank()) +
+  labs(title = 'What do we currently use R for?') +
+  scale_y_continuous(breaks = scales::pretty_breaks())
+
+ggsave(file.path('R_form', 'current_R_use.pdf'), last_plot(), width = 5.5, height = 5)
+ggsave(file.path('R_form', 'current_R_use.png'), last_plot(), width = 5.5, height = 5)
+
+# time spent on R ####
+R_time <- select(d_use, hours_week, name) %>%
+  mutate(., hours_week = as.numeric(hours_week),
+         x = 'a')
+
+ggplot(R_time, aes(hours_week)) +
+  geom_density()
